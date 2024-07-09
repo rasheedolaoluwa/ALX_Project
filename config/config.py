b2b481f1f4c80dc744e1439c04d8bb4c0706b70c
rasheed <radeyola@gmail.com>
2024-07-09 15:43:26 +0000

    deployment issues resolved

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Flask-Mail Configuration
